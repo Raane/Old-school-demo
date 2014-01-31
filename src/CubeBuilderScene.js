b9ff58f2fa8ef0402a8f632082da3093e0e1717d
Rune Holmgren <Raane.Holm@gmail.com>
2014-02-01 00:27:12 +0100

    Add code to draw a thin 3D rectangle

--- src/CubeBuilderScene.js
+++ src/CubeBuilderScene.js
@@ -12,15 +12,22 @@ CubeBuilderScene.prototype.init = function(cb){
     this.camera = new THREE.PerspectiveCamera(45, 16/9, 0.1, 10000);
     this.scene.add(this.camera);
 
-    var sphereMaterial = new THREE.MeshLambertMaterial({color: 0xCC0000});
-    var material = new THREE.MeshBasicMaterial( { color: 0x666666, wireframe: true, wireframeLinewidth: 4 } );
+    var material = new THREE.MeshBasicMaterial( { color: 0xFFFFFF, wireframe: false, wireframeLinewidth: 4000 } );
 
-    this.sphere = new THREE.Mesh( new THREE.SphereGeometry(
-        50, 16, 16), material);
+    this.cubes = new Array();
+    
+    for(var i=0;i<12;i++) {
+        this.cubes[i] = new THREE.Mesh( new THREE.CubeGeometry(
+        GU, GU, GU), material);
+        this.cubes[i].position.x = -2*GU;
+        this.cubes[i].position.y = 2*GU;
+        this.cubes[i].position.z = 2*GU;
+        this.cubes[i].scale.set(0.1,0.1,0.0001);
+        this.scene.add(this.cubes[i]);
+    }
 
-    glob = this.sphere;
+    tmpCube = this.cubes[0];
 
-    this.scene.add(this.sphere);
 
 
     /* call cb when you are done loading! */
@@ -30,11 +37,37 @@ CubeBuilderScene.prototype.init = function(cb){
 CubeBuilderScene.prototype.reset = function(){
     /* reset all the variables! */
 
-    this.camera.position.z = 300;
+    this.camera.position.z = 8*GU;
 }
 
 CubeBuilderScene.prototype.update = function(){
-    this.sphere.rotation.x = Math.sin(t/100);
+    if(t>this.startTime && t<this.startTime+5000) {
+        var cubesToScale = [0,2,4,6];
+        for(var i=0;i<cubesToScale.length;i++) {
+            this.cubes[cubesToScale[i]].position.y = 2*GU-t*4*GU/(5000*2);
+            this.cubes[cubesToScale[i]].scale.set(0.1, t*4.1/5000, 0.0001);
+        }
+        var cubesToMove = [3,8,7,11]
+        for(var i=0;i<cubesToMove.length;i++) {
+            this.cubes[cubesToMove[i]].position.y = 2*GU-t*4*GU/(5000);
+        }
+    }
+    if(t>this.startTime+5000 && t<this.startTime+10000) {
+        var cubesToScale = [1,3,5,7];
+        for(var i=0;i<cubesToScale.length;i++) {
+            this.cubes[cubesToScale[i]].position.x = -2*GU+(t-5000)*4*GU/(5000*2);
+            this.cubes[cubesToScale[i]].scale.set((t-5000)*4.1/5000, 0.1, 0.0001);
+        }
+        var cubesToMove = [2,10,11,6]
+        for(var i=0;i<cubesToMove.length;i++) {
+            this.cubes[cubesToMove[i]].position.x = -2*GU+(t-5000)*4*GU/(5000);
+        }
+
+        this.cubes[2].position.x = -2*GU+(t-5000)*4*GU/(5000);
+    }
+    if(t>this.startTime+10000 && t<this.startTime+15000) {
+
+    }
     /* do updatey stuff here */
 }
 
